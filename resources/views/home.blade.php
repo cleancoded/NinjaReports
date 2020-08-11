2c2ebf17f8753fa1005ea8785c5271b648ab5d84
James <james@cleancoded.com>
2020-08-11 19:35:45 +0500

    html init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.master')
+@section('title', 'Home')
+@section('content')
+
+<div class="col-md-10  overview">
+    <div class="row audit-text pt-3 pb-3">
+        <div class="col-md-12 text-start">
+            <h5 style="margin-left: 22px;"><STRONG>Welcome Tyler!</STRONG></h5>
+        </div>
+    </div>
+    <div class="row Welcome-two-cols">
+        <div class="col-md-6 text-center Quick-col">
+            <h5>QUICK ANALYSIS</h5>
+            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical</p>
+                <div class="row">
+                    <div class="col-md-10">
+                        <input type="text" class="form-control" placeholder="Analyze">
+                    </div>
+                    <div class="col-md-2">
+                        <button class="btn btn-primary">Analyze</button>
+                    </div>
+                </div>
+        </div>
+        <div class="col-md-6 plan-col">
+            <h6><span>Plan:</span><span>Free</span></h6>
+            <h6><span>Next Billing Date</span><span>N/A</span></h6>
+            <button class="btn btn-warning">upgrade</button>
+        </div>
+    </div>
+</div>
+
+@endsection
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,14 @@
+	<div class="row top-bar">
+		<div class="col-md-6 text-left">
+			<h5>Ninja Reports</h5>
+		</div>
+
+		<div class="col-md-6 text-right">
+			<a class="dropdown-toggle" data-toggle="dropdown" href="#"><img src="{{asset('images/profile.jpg')}}" style="height: 30px;border-radius: 50%;"></a>
+			<div class="dropdown-menu">
+				<a class="dropdown-item" href="#"><span><img src="{{asset('images/black.jpg')}}" style="height: 10px;width: 10px;margin-right: 5px;"></span>My Account</a>
+				<a class="dropdown-item" href="#"><span><img src="{{asset('images/black.jpg')}}" style="height: 10px;width: 10px;margin-right: 5px;"></span>Sign Out</a>
+			</div>
+		</div>
+
+    </div>
